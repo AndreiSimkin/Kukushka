b64311d5642b4f24bd2373527fa48cea7087696b
AndreiSimkin <62541520+AndreiSimkin@users.noreply.github.com>
2021-05-17 00:17:39 +0300

    Добавил библиотеки

--- Kukushka.Talk/Views/Main/Messenger.xaml.cs
+++ Kukushka.Talk/Views/Main/Messenger.xaml.cs
@@ -25,5 +25,21 @@ namespace Kukushka.Talk.Views.Main
         {
             InitializeComponent();
         }
+
+        private void MessengerWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Keyboard.FocusedElement != null)
+            {
+                var textBox = (FrameworkElement)Keyboard.FocusedElement;
+                FrameworkElement parent = (FrameworkElement)textBox.Parent;
+                while (parent != null && parent is IInputElement && !((IInputElement)parent).Focusable)
+                {
+                    parent = (FrameworkElement)parent.Parent;
+                }
+
+                DependencyObject scope = FocusManager.GetFocusScope(textBox);
+                FocusManager.SetFocusedElement(scope, parent as IInputElement);
+            }
+        }
     }
 }
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+using Kukushka.Network.Models;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kukushka.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetActivityTimeTest()
+        {
+            var user = new User();
+            user.SetActivity(System.DayOfWeek.Sunday, 1, true);
+            user.SetActivity(System.DayOfWeek.Sunday, 2, true);
+            user.SetActivity(System.DayOfWeek.Sunday, 3, true);
+            user.SetActivity(System.DayOfWeek.Sunday, 4, true);
+            user.SetActivity(System.DayOfWeek.Sunday, 5, true);
+            user.SetActivity(System.DayOfWeek.Sunday, 6, true);
+
+            var days = user.ActivityTime;
+
+            TestContext.Write("**");
+            for (int i = 0; i < 7; i++)
+                TestContext.Write("		" + new string[] { "бя", "ом", "бр", "яп", "вр", "ор", "яа" }[i]);
+            TestContext.WriteLine();
+
+
+            for (int h = 0; h < 24; h++)
+            {
+                TestContext.Write((h + 1).ToString("D2"));
+                for (int d = 0; d < 7; d++)
+                    TestContext.Write("		" + (days[(DayOfWeek)d][h] ? "ON" : "OF"));
+                TestContext.WriteLine();
+            }
+
+            Assert.Pass();
+
+        }
+    }
+}
